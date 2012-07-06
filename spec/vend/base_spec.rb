5ed5773743a3265504d2bed157b009a27d5435fb
Malcolm Locke <malc@wholemeal.co.nz>
2012-07-06 14:10:38 +1200

    Re-organise Vend::Base specs

--- spec/vend/base_spec.rb
+++ spec/vend/base_spec.rb
@@ -21,11 +21,6 @@ describe Vend::Base do
     subject.should be_instance_of(Vend::Resource::Foo)
   end
 
-  it "builds a Foo" do
-    Vend::Resource::Foo.build(client, :attrs => attribute_hash).should
-      be_instance_of(Vend::Resource::Foo)
-  end
-
   it "assigns the client" do
     subject.client.should == client
   end
@@ -34,76 +29,119 @@ describe Vend::Base do
     subject.attrs.should == attribute_hash
   end
 
-  it "parses JSON" do
-    described_class.parse_json('{"baz":"baloo"}').should == {"baz" => "baloo"}
+  describe '.build' do
+    it "builds a Foo" do
+      Vend::Resource::Foo.build(client, :attrs => attribute_hash).should
+        be_instance_of(Vend::Resource::Foo)
+    end
   end
 
-  it "raises an exception with invalid JSON" do
-    expect {
-      described_class.parse_json('foo')
-    }.to raise_error(Vend::Resource::InvalidResponse)
+  describe '.parse_json' do
+
+    it "parses JSON" do
+      described_class.parse_json('{"baz":"baloo"}').should == {"baz" => "baloo"}
+    end
+
+    it "raises an exception with invalid JSON" do
+      expect {
+        described_class.parse_json('foo')
+      }.to raise_error(Vend::Resource::InvalidResponse)
+    end
+
   end
 
-  it "initializes a singular resource from JSON results" do
-    resource = Vend::Resource::Foo.initialize_singular(client,mock_response)
-    resource.should be_a Vend::Resource::Foo
-    resource.bar.should == "baz"
+  describe '.initialize_singular' do
+
+    it "initializes a singular resource from JSON results" do
+      resource = Vend::Resource::Foo.initialize_singular(client,mock_response)
+      resource.should be_a Vend::Resource::Foo
+      resource.bar.should == "baz"
+    end
+
   end
 
-  it "initializes a collection from JSON results" do
-    collection = Vend::Resource::Foo.initialize_collection(client, mock_response)
-    a_foo = collection.first
-    a_foo.should be_a Vend::Resource::Foo
-    a_foo.bar.should == "baz"
+  describe '.initialize_collection' do
+
+    it "initializes a collection from JSON results" do
+      collection = Vend::Resource::Foo.initialize_collection(client, mock_response)
+      a_foo = collection.first
+      a_foo.should be_a Vend::Resource::Foo
+      a_foo.bar.should == "baz"
+    end
+
   end
 
-  it "returns the endpoint name" do
-    Vend::Resource::Foo.endpoint_name.should == 'foo'
+  describe '.endpoint_name' do
+
+    it "returns the endpoint name" do
+      Vend::Resource::Foo.endpoint_name.should == 'foo'
+    end
+
   end
 
-  it "returns the collection name" do
-    Vend::Resource::Foo.collection_name.should == 'foos'
+  describe '.collection_name' do
+
+    it "returns the collection name" do
+      Vend::Resource::Foo.collection_name.should == 'foos'
+    end
+
   end
 
-  it "finds a Foo by id" do
-    mock_response = '{"foos":[{"id":"1","bar":"baz"}]}'
-    response = mock
-    response.should_receive(:body).and_return(mock_response)
-    client.should_receive(:request).with('foos', :id => "1").and_return(response)
-    foo = Vend::Resource::Foo.find(client, "1")
-    foo.should be_instance_of(Vend::Resource::Foo)
-    foo.bar.should == "baz"
+  describe '.find' do
+
+    it "finds a Foo by id" do
+      mock_response = '{"foos":[{"id":"1","bar":"baz"}]}'
+      response = mock
+      response.should_receive(:body).and_return(mock_response)
+      client.should_receive(:request).with('foos', :id => "1").and_return(response)
+      foo = Vend::Resource::Foo.find(client, "1")
+      foo.should be_instance_of(Vend::Resource::Foo)
+      foo.bar.should == "baz"
+    end
+
   end
 
-  it "returns all Foo objects" do
-    response = mock
-    response.should_receive(:body).and_return(mock_response)
-    client.should_receive(:request).with('foos').and_return(response)
-    foos = Vend::Resource::Foo.all(client)
-    foos.length.should == 2
-    foos.first.should be_instance_of(Vend::Resource::Foo)
-    foos.first.bar.should == "baz"
+  describe '.all' do
+
+    it "returns all Foo objects" do
+      response = mock
+      response.should_receive(:body).and_return(mock_response)
+      client.should_receive(:request).with('foos').and_return(response)
+      foos = Vend::Resource::Foo.all(client)
+      foos.length.should == 2
+      foos.first.should be_instance_of(Vend::Resource::Foo)
+      foos.first.bar.should == "baz"
+    end
+
   end
 
-  it "returns all Foo objects that have been modified since a Time" do
-    time = Time.new(2012,5,8)
-    response = mock
-    response.should_receive(:body).and_return(mock_response)
-    client.should_receive(:request).with('foos', :since => time).and_return(response)
-    foos = Vend::Resource::Foo.since(client, time)
-    foos.length.should == 2
-    foos.first.should be_instance_of(Vend::Resource::Foo)
-    foos.first.bar.should == "baz"
+  describe '.since' do
+
+    it "returns all Foo objects that have been modified since a Time" do
+      time = Time.new(2012,5,8)
+      response = mock
+      response.should_receive(:body).and_return(mock_response)
+      client.should_receive(:request).with('foos', :since => time).and_return(response)
+      foos = Vend::Resource::Foo.since(client, time)
+      foos.length.should == 2
+      foos.first.should be_instance_of(Vend::Resource::Foo)
+      foos.first.bar.should == "baz"
+    end
+
   end
 
-  it "returns all Foo objects that belong to an outlet" do
-    response = mock
-    response.should_receive(:body).and_return(mock_response)
-    client.should_receive(:request).with('foos', :outlet_id => 'outlet').and_return(response)
-    foos = Vend::Resource::Foo.outlet_id(client, 'outlet')
-    foos.length.should == 2
-    foos.first.should be_instance_of(Vend::Resource::Foo)
-    foos.first.bar.should == "baz"
+  describe '.outlet_id' do
+
+    it "returns all Foo objects that belong to an outlet" do
+      response = mock
+      response.should_receive(:body).and_return(mock_response)
+      client.should_receive(:request).with('foos', :outlet_id => 'outlet').and_return(response)
+      foos = Vend::Resource::Foo.outlet_id(client, 'outlet')
+      foos.length.should == 2
+      foos.first.should be_instance_of(Vend::Resource::Foo)
+      foos.first.bar.should == "baz"
+    end
+
   end
 
   describe "dynamic instance methods" do
@@ -122,7 +160,7 @@ describe Vend::Base do
     end
   end
 
-  describe "searching" do
+  describe ".search" do
 
     it "returns an array of Foo objects" do
       mock_response = '{
@@ -139,30 +177,41 @@ describe Vend::Base do
       foos = Vend::Resource::Foo.search(client, :bar, 'baz')
       foos.first.should be_a Vend::Resource::Foo
     end
+
   end
 
-  describe "deleting" do
-    specify "delete! deletes an object" do
-      objekt = Vend::Resource::Foo.new(client, :attrs => {'id' => 1} )
-      client.should_receive(:request).with('foos', :method => :delete, :id => 1)
+  describe "delete!" do
 
-      objekt.delete!
+    context "when id is present" do
+      subject { Vend::Resource::Foo.new(client, :attrs => {'id' => 1}) }
+
+      it "deletes the object" do
+        client.should_receive(:request).with('foos', :method => :delete, :id => 1)
+        subject.delete!
+      end
     end
 
-    specify "delete! throws an error when no id is present" do
-      objekt = Vend::Resource::Foo.new(client, :attrs => {:foo => 'bar'})
-      client.should_not_receive(:request)
+    context "when id is absent" do
+      subject { Vend::Resource::Foo.new(client, :attrs => {:foo => 'bar'}) }
 
-      expect {
-        objekt.delete!
-      }.to raise_error(Vend::Resource::IllegalAction, "Vend::Resource::Foo has no unique ID")
+      it "raises Vend::Resource::IllegalAction" do
+        client.should_not_receive(:request)
+        expect {
+          subject.delete!
+        }.to raise_error(Vend::Resource::IllegalAction, "Vend::Resource::Foo has no unique ID")
+      end
     end
 
-    specify "delete returns false when no id is present" do
+  end
+
+  describe 'delete' do
+
+    it "returns false when no id is present" do
       objekt = Vend::Resource::Foo.new(client, :attrs => {:foo => 'bar'})
       client.should_not_receive(:request)
       objekt.delete.should be_false
     end
+
   end
 
   describe ".paginates?" do
